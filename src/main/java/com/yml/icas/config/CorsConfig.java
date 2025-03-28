3912e81ad815c4073d3069c7e04fa7c23f0b0d09
Lakshakumara <lakshakumara@gmail.com>
2025-03-28 15:51:54 +0530

    2025/3/28

--- src/main/java/com/yml/icas/config/CorsConfig.java
+++ src/main/java/com/yml/icas/config/CorsConfig.java
@@ -1,19 +1,20 @@
 package com.yml.icas.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-//@Configuration
+@Configuration
 public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // Allow Angular frontend
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://mediclaim.ousl.lk")); // Allow Angular frontend
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true); // Required if using authentication
